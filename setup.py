b2dcef36d994377c8e0811183a4aafa8eb756f6e
Kochnev Stepan <ksa@sibdigital.net>
2023-04-21 23:10:14 +0800

    fix

--- setup.py
+++ setup.py
@@ -4,5 +4,4 @@ setup(
     name='irisqualifier-method',
     version='0.0.1',
     packages=find_packages(),
-)from setuptools import setup, find_packages
-setup(name='irisqualifier-method', version='0.0.1', packages=find_packages())
+)
